2da0b228d27d3fbb73b50daf5a4ddb38687b058b
David Ozokoye <davidozokoye@gmail.com>
2020-07-14 15:01:41 +0100

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,197 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import data from "./Data";
+import Context from "./Context";
+import Login from "./components/Login";
+import AddProduct from "./components/AddProduct";
+import Cart from "./components/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      products: [],
+      Cart: {},
+    };
+    this.routerRef = React.createRef();
+  }
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    let products = localStorage.getItem("products");
+
+    user = user ? JSON.parse(user) : null;
+    products = products ? JSON.parse(products) : data.initProducts;
+
+    this.setState({ user, products });
+  }
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    localStorage.setItem("products", JSON.stringify(products));
+    this.setState({ products }, () => callback && callback());
+  };
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    let products = localStorage.getItem("products");
+    let cart = localStorage.getItem("cart");
+
+
+    products = products ? JSON.parse(products) : data.initProducts;
+    user = user ? JSON.parse(user) : null;
+    cart = cart? JSON.parse(cart) : {};
+
+    this.setState({ user, products, cart });
+  }
+
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
+    }
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  removeFromCart = cartItemId => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+  clearCart = () => {
+    let cart = {};
+    localStorage.removeItem("cart");
+    this.setState({ cart });
+  };
+
+  checkout = () => {
+    if (!this.state.user) {
+      this.routerRef.current.history.push("/login");
+      return;
+    }
+    const cart = this.state.cart;
+    const products = this.state.products.map(p => {
+      if (cart[p.name]) {
+        p.stock = p.stock - cart[p.name].amount;
+      }
+      return p;
+    });
+    this.setState({ products });
+    this.clearCart();
+  };
+
+  login = (usn, pwd) => {
+    let user = data.users.find(u => u.username === usn && u.password === pwd);
+    if (user) {
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    }
+    return false;
+  };
+
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+  
+  
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className="App">
+            <nav
+              className="navbar container"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div className="navbar-brand">
+                <b className="navbar-item is-size-4 ">ecommerce</b>
+                <a
+                  role="button"
+                  class="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>
+              </div>
+              <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    {Object.keys(this.state.cart).length}
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
+                ) : (
+                  <a className="navbar-item" onClick={this.logout}>
+                    Logout
+                  </a>
+                )}
+              </div>
+            </nav>
+            <Switch>
+            <Route exact path="/" component={Component} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={Component} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
 }
-
-export default App;
---
+++ src/Data.js
@@ -0,0 +1,48 @@
+export default {
+    users: [
+      { username: "regular", accessLevel: 1, password: "password" },
+      { username: "admin", accessLevel: 0, password: "password" }
+    ],
+    initProducts: [
+      {
+        name: " Nike shoes",
+        stock: 10,
+        price: 399.99,
+        shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+        description:
+          "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam          elis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+      },
+      {
+        name: "Shirt",
+        stock: 14,
+        price: 99.99,
+        shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+        description:
+          "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam          elis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+      },
+      {
+        name: "Leather Jacks",
+        stock: 7,
+        price: 499.99,
+        shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+        description:
+          "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam          elis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+      },
+      {
+        name: "Loafers",
+        stock: 19,
+        price: 159.99,
+        shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+        description:
+          "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam          elis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+      },
+      {
+        name: "Joggers",
+        stock: 11,
+        price: 659.99,
+        shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+        description:
+          "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam          elis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+      },
+    ]
+  };
